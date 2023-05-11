485500cc60c249ea931e524c7e571fa5990b710e
MidnightTale <midnighttale24@gmail.com>
2023-05-12 04:31:15 +0700

    remove backup task on start

--- src/main/java/xyz/hynse/hynsebackup/HynseBackup.java
+++ src/main/java/xyz/hynse/hynsebackup/HynseBackup.java
@@ -11,20 +11,11 @@ public class HynseBackup extends JavaPlugin {
 
     private BossBar backupProgressBossBar;
     private BackupConfig backupConfig;
-    private BackupManager backupManager;
-
     @Override
     public void onEnable() {
         saveDefaultConfig();
         backupConfig = new BackupConfig(getConfig());
         backupProgressBossBar = Bukkit.createBossBar("Backup in progress...", BarColor.BLUE, BarStyle.SEGMENTED_10);
-        backupManager = new BackupManager(this, backupConfig, backupProgressBossBar);
-
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                backupManager.backupWhitelistedWorlds();
-            }
-        }.runTaskAsynchronously(this);
+        new BackupManager(this, backupConfig, backupProgressBossBar);
     }
 }
